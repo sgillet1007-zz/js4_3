28f4b715a7feab5c0e2d02f0a7d0bb729b626e75
sgillet1007 <samuelgillet@hotmail.com>
2015-06-26 12:06:25 -0600

    finished popup with delegated JQ behvior on close button

--- main.js
+++ main.js
@@ -1,5 +1,11 @@
 $(document).on('ready', function() {
 
-	
-  
+$('.button-giant').on('click',function(){
+	$('.button-giant').before('<div class="popup"> <p class="popup-item">~</p><button class="popup-item myButton">Close</button></div>');
+})
+
+$('.container').on('click', '.myButton', function(){
+	$('.popup').css('display','none')
+})
+
 });
